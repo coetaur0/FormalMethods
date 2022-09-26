aa7f62e1b351334379143916a1f34d6b3ff35fff
coetaur0 <aurelien.coet19@gmail.com>
2022-09-26 10:05:31 +0200

    Implement exercise 2

---
+++ Exercises/Exercise2/Exercise/Program.fs
@@ -0,0 +1,32 @@
+﻿open PetriNet
+
+/// The places of the Petri net representing a traffic light.
+type Place =
+    | Red
+    | Yellow
+    | Green
+
+/// The transitions of the Petri net representing a traffic light.
+type Transition =
+    | GreenToYellow
+    | YellowToRed
+    | RedToGreen
+
+/// The Petri net's pre-conditions.
+let pre =
+    Arcs.make [ ((Green, GreenToYellow), 1)
+                ((Yellow, YellowToRed), 1)
+                ((Red, RedToGreen), 1) ]
+
+/// The Petri net's post-conditions.
+let post =
+    Arcs.make [ ((Yellow, GreenToYellow), 1)
+                ((Red, YellowToRed), 1)
+                ((Green, RedToGreen), 1) ]
+
+/// The Petri net model representing a traffic light.
+let model = Model.make pre post
+
+/// The Petri net's initial marking.
+let initialMarking =
+    Marking.make [ (Red, 1) ]
