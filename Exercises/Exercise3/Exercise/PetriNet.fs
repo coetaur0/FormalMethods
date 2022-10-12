26f11cce9a55860b6a09726a15fd623cb442d965
coetaur0 <aurelien.coet19@gmail.com>
2022-10-12 16:16:41 +0200

    Implement exercise 3

---
+++ Exercises/Exercise3/Exercise/PetriNet.fs
@@ -0,0 +1,149 @@
+﻿module PetriNet
+
+open Microsoft.FSharp.Reflection
+
+// ----- Utilities -------------------------------------------------------------------------------------------------- //
+
+/// Returns the set of all the cases in a discriminated union.
+let private getAllCases<'T when 'T: comparison> () =
+    if not (FSharpType.IsUnion typeof<'T>) then
+        failwith "The types for places and transitions must be discriminated unions."
+
+    FSharpType.GetUnionCases typeof<'T>
+    |> Seq.map (fun case -> FSharpValue.MakeUnion(case, [||]) :?> 'T)
+    |> Set.ofSeq
+
+/// Shuffles the elements in a list.
+let shuffle (list: 'a list) : 'a list =
+    let rnd = System.Random()
+    let length = List.length list
+    List.sortBy (fun _ -> rnd.Next(1, length)) list
+
+// ----- Token ------------------------------------------------------------------------------------------------------ //
+
+/// Tokens in a Petri net are simply represented by integer counts.
+type Token = int
+
+// ----- Marking ---------------------------------------------------------------------------------------------------- //
+
+/// A marking is a mapping from places to token counts.
+type Marking<'Place when 'Place: comparison> =
+    private
+        { Counts: Map<'Place, Token> }
+
+    /// Returns the token count associated with a given place in a marking.
+    member this.Item(place: 'Place) : Token = Map.find place this.Counts
+
+    /// Returns a string representation for a marking.
+    override this.ToString() : string =
+        let repr =
+            this.Counts
+            |> Map.fold (fun str place tokens -> str + $"{place}: {tokens}, ") "{"
+
+        repr[0 .. repr.Length - 3] + "}"
+
+[<RequireQualifiedAccess>]
+module Marking =
+
+    /// Returns a new marking built from a sequence of mappings from places to token counts.
+    let make (mappings: seq<'Place * Token>) : Marking<'Place> =
+        // A marking is a total map, meaning that for every place in a Petri net, there is a token count in the marking.
+        // Every place that isn't initialised with a specific value is given a default token count of zero.
+        let totalMap =
+            getAllCases<'Place> ()
+            |> Set.fold (fun zeroMap place -> Map.add place 0 zeroMap) Map.empty
+
+        let counts =
+            mappings
+            |> Seq.fold (fun countMap (place, tokens) -> Map.add place tokens countMap) totalMap
+
+        { Counts = counts }
+
+    /// Returns a new marking where the token count associated with a given place has been updated to a new value.
+    let where (place: 'Place) (count: Token) (marking: Marking<'Place>) : Marking<'Place> =
+        { Counts = Map.add place count marking.Counts }
+
+// ----- Arcs ------------------------------------------------------------------------------------------------------- //
+
+/// The pre- and post-condition functions of a Petri net are represented by sets of weighted arcs from places to
+/// transitions. They are implemented as mappings from pairs of places and transitions to token counts.
+type Arcs<'Place, 'Transition when 'Place: comparison and 'Transition: comparison> =
+    private
+        { Values: Map<'Place * 'Transition, Token> }
+
+    /// Returns the token count associated with a pair of nodes in a set of arcs.
+    member this.Item(arc: 'Place * 'Transition) : Token =
+        match Map.tryFind arc this.Values with
+        | Some count -> count
+        | None -> 0
+
+[<RequireQualifiedAccess>]
+module Arcs =
+
+    /// Returns a new set of arcs built from a sequence of mappings from pairs of nodes to token counts.
+    let make (mappings: seq<('Place * 'Transition) * Token>) : Arcs<'Place, 'Transition> = { Values = Map(mappings) }
+
+// ----- Model ------------------------------------------------------------------------------------------------------ //
+
+/// A Petri net is represented by the types of its places and transitions, as well as its pre- and post-condition
+/// functions. The pre- and post-condition functions are implemented as sets of arcs.
+type Model<'Place, 'Transition when 'Place: comparison and 'Transition: comparison> =
+    private
+        { Places: Set<'Place>
+          Transitions: Set<'Transition>
+          Pre: Arcs<'Place, 'Transition>
+          Post: Arcs<'Place, 'Transition> }
+
+[<RequireQualifiedAccess>]
+module Model =
+
+    /// Returns a new model built from pre- and post-condition functions expressed as sets of arcs.
+    let make (pre: Arcs<'Place, 'Transition>) (post: Arcs<'Place, 'Transition>) : Model<'Place, 'Transition> =
+        { Places = getAllCases<'Place> ()
+          Transitions = getAllCases<'Transition> ()
+          Pre = pre
+          Post = post }
+
+    /// Returns the set of places in a model.
+    let places (model: Model<'Place, 'Transition>) : Set<'Place> = model.Places
+
+    /// Returns the set of transitions in a model.
+    let transitions (model: Model<'Place, 'Transition>) : Set<'Transition> = model.Transitions
+
+    /// Fires a transition from a given marking in a model.
+    let fire
+        (model: Model<'Place, 'Transition>)
+        (marking: Marking<'Place>)
+        (transition: 'Transition)
+        : Option<Marking<'Place>> =
+        model.Places
+        |> Set.fold
+            (fun newMarking place ->
+                let newCount =
+                    marking[place]
+                    - model.Pre[place, transition]
+                    + model.Post[place, transition]
+
+                Marking.where place newCount newMarking)
+            marking
+        |> Some
+
+    /// Simulates the execution of a model for a given number of steps.
+    let rec simulate (model: Model<'Place, 'Transition>) (marking: Marking<'Place>) (steps: int) : Marking<'Place> =
+        if steps <= 0 then
+            marking
+        else
+            let nextMarking =
+                model.Transitions
+                |> Set.toList
+                |> shuffle
+                |> List.tryPick (fun t ->
+                    let m = (fire model marking t).Value
+
+                    match Map.forall (fun _ tokens -> tokens >= 0) m.Counts with
+                    | true -> Some m
+                    | false -> None)
+
+            match nextMarking with
+            | Some m -> simulate model m (steps - 1)
+            | None -> marking
---
+++ Exercises/Exercise3/Exercise/Program.fs
@@ -0,0 +1,40 @@
+﻿open PetriNet
+
+type Place =
+    | P0
+    | P1
+    | P2
+    | P3
+    | P4
+
+type Transition =
+    | T0
+    | T1
+    | T2
+    | T3
+    | T4
+
+let pre =
+    Arcs.make [ ((P0, T2), 1)
+                ((P1, T3), 1)
+                ((P2, T2), 1)
+                ((P3, T3), 1)
+                ((P4, T4), 18) ]
+
+let post =
+    Arcs.make [ ((P0, T3), 1)
+                ((P1, T2), 1)
+                ((P2, T0), 1)
+                ((P3, T1), 1)
+                ((P4, T2), 6)
+                ((P4, T3), 9) ]
+
+let model = Model.make pre post
+
+let initialMarking =
+    Marking.make [ (P0, 1) ]
+
+let markingAfter100 =
+    Model.simulate model initialMarking 100
+
+printfn $"Marking after firing a 100 transitions: {markingAfter100}"
