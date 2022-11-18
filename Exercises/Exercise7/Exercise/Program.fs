ef685c59bfe19d51a3e49d50cf5b1d7a54de2728
coetaur0 <aurelien.coet19@gmail.com>
2022-11-18 10:58:49 +0100

    Write the solution for exercise 7

--- Exercises/Exercise7/Exercise/Program.fs
+++ Exercises/Exercise7/Exercise/Program.fs
@@ -1,19 +1,73 @@
 ﻿open MathNet.Numerics.LinearAlgebra
 
-let M0 =
-    matrix [ [ 1.0; 1.0; 1.0 ]
-             [ 2.0; 2.0; 2.0 ]
-             [ 3.0; 3.0; 3.0 ] ]
+// ----- Exercise 1 ------------------------------------------------------------------------------------------------- //
 
-let M1 =
-    matrix [ [ 3.0; 2.0; 0.0 ]
-             [ 1.0; 0.0; 1.0 ]
-             [ 0.0; 3.0; 1.0 ] ]
+printfn "Exercise 1:"
 
-printfn $"M0 + M1 = {M0 + M1}"
+let input =
+    matrix [ [ 1.0; 0.0; 0.0; 0.0 ]
+             [ 0.0; 1.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 0.0; 0.0; 1.0 ]
+             [ 1.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 0.0; 1.0; 0.0 ]
+             [ 1.0; 0.0; 0.0; 0.0 ] ]
 
-let v = vector [ 1.0; 2.0; 1.0 ]
+let output =
+    matrix [ [ 0.0; 1.0; 0.0; 0.0 ]
+             [ 1.0; 0.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 0.0; 1.0 ]
+             [ 0.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 1.0; 0.0; 1.0 ]
+             [ 1.0; 0.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 1.0; 0.0 ] ]
 
-printfn $"M1 * v = {M1 * v}"
+let incidence = output - input
 
-printfn $"v * M1 = {v * M1}"
+printfn $"Incidence matrix: {incidence}"
+
+let initialMarking =
+    vector [ 1.0
+             0.0
+             1.0
+             0.0
+             1.0
+             0.0
+             3.0 ]
+
+let s0 = vector [ 3.0; 3.0; 1.0; 1.0 ]
+
+printfn $"Marking after firing s0 = {initialMarking + incidence * s0}"
+
+let s1 = vector [ 1.0; 1.0; 2.0; 1.0 ]
+
+printfn $"Marking after firing s1 = {initialMarking + incidence * s1}"
+
+// ----- Exercise 2 ------------------------------------------------------------------------------------------------- //
+
+printfn "\nExercise 2:"
+
+let input2 =
+    matrix [ [ 0.0; 0.0; 1.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 0.0; 1.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 0.0; 0.0; 0.0; 18.0 ] ]
+
+let output2 =
+    matrix [ [ 0.0; 0.0; 0.0; 1.0; 0.0 ]
+             [ 0.0; 0.0; 1.0; 0.0; 0.0 ]
+             [ 1.0; 0.0; 0.0; 0.0; 0.0 ]
+             [ 0.0; 1.0; 0.0; 0.0; 0.0 ]
+             [ 0.0; 0.0; 6.0; 9.0; 0.0 ] ]
+
+let incidence2 = output2 - input2
+
+printfn $"Incidence matrix: {incidence2}"
+
+let initialMarking2 =
+    vector [ 1.0; 0.0; 0.0; 0.0; 0.0 ]
+
+let s2 = vector [ 3.0; 2.0; 3.0; 2.0; 2.0 ]
+
+printfn $"Marking after firing s2 = {initialMarking2 + incidence2 * s2}"
