419cef35447085888581a285a876a0cd339693d7
coetaur0 <aurelien.coet19@gmail.com>
2022-11-18 10:29:52 +0100

    Implement exercise 7

---
+++ Exercises/Exercise7/Exercise/Program.fs
@@ -0,0 +1,19 @@
+﻿open MathNet.Numerics.LinearAlgebra
+
+let M0 =
+    matrix [ [ 1.0; 1.0; 1.0 ]
+             [ 2.0; 2.0; 2.0 ]
+             [ 3.0; 3.0; 3.0 ] ]
+
+let M1 =
+    matrix [ [ 3.0; 2.0; 0.0 ]
+             [ 1.0; 0.0; 1.0 ]
+             [ 0.0; 3.0; 1.0 ] ]
+
+printfn $"M0 + M1 = {M0 + M1}"
+
+let v = vector [ 1.0; 2.0; 1.0 ]
+
+printfn $"M1 * v = {M1 * v}"
+
+printfn $"v * M1 = {v * M1}"
